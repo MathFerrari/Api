2c61d8a14a3d8ae2f3a82dd2e4ac3b6ed8767a76
Matheus Ferrari <matheusap.ferrari@hotmail.com>
2024-04-12 22:26:01 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ import express from 'express';
 const route = express.Router();
 
 
-import { works, createWork } from './src/controllers/worksController.js';
-import { login } from './src/controllers/LoginController.js';
-import { experiences, createExperience } from './src/controllers/ExperienceController.js';
+import { works, createWork } from './controllers/worksController.js';
+import { login } from './controllers/LoginController.js';
+import { experiences, createExperience } from './controllers/ExperienceController.js';
 
-import { checkToken } from './src/middlewares/middleware.js';
+import { checkToken } from './middlewares/middleware.js';
 
 //telas inciais
 route.post('/login', login)
